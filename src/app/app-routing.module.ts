0e879b5c44f1b5e5c0249bae229f2e16057ac341
Onkar Adlinge <onkar.adlinge@BBD.INTL>
2023-06-05 15:12:11 +0530

    commiting all files to be saved

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:MainLayoutComponent,
+  loadChildren:()=>import("./layout/layout.module").then((m)=> m.LayoutModule)  
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
